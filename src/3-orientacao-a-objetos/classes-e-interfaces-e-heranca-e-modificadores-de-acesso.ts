c3c036a7cdcc5570551cf40bbc56369836ec108c
matheustakasaki <matheus8antunes@gmail.com>
2022-06-26 12:08:40 -0300

    Aprendendo sobre Union Types: type alias, union type e intersection types

---
+++ src/3-orientacao-a-objetos/classes-e-interfaces-e-heranca-e-modificadores-de-acesso.ts
@@ -0,0 +1,18 @@
+// Classes e funções
+// class User {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
+// class Player extends User {
+//     constructor(name, age, game) {
+//         super(name, age);
+
+//         this.game = game;
+
+//     }
+// }
+// const jogador = new Player('Matheus', 20, 'Guardians of the Galaxy');
+// console.log(jogador)
