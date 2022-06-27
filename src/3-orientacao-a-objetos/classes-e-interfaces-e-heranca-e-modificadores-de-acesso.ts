222e138c04913c003d6c1b1b423b203a1f98b9c6
Matheus <matheus@tdzain.com>
2022-06-27 10:29:31 -0300

    Aprendendo sobre Encapsulamento, Interfaces e métodos estáticos

--- src/3-orientacao-a-objetos/classes-e-interfaces-e-heranca-e-modificadores-de-acesso.ts
+++ src/3-orientacao-a-objetos/classes-e-interfaces-e-heranca-e-modificadores-de-acesso.ts
@@ -1,18 +1,89 @@
 // Classes e funções
-// class User {
-//     constructor(name, age) {
-//         this.name = name;
-//         this.age = age;
-//     }
+class UserExample {
+  public name;
+  public age;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Player extends UserExample {
+  public game;
+
+  constructor(name: string, age: number, game: string) {
+    super(name, age);
+
+    this.game = game;
+  }
+
+  //   Método não estático - Acessível somente na instância
+  dizerOJogoAtual() {
+    return `Estou jogando no momento: ${this.game}`;
+  }
+
+  //   Método Estático - Acessível a partir da classe
+  static queHorasSao() {}
+}
+const jogador = new Player("Matheus", 20, "Guardians of the Galaxy");
+console.log(jogador);
+
+console.log(jogador.dizerOJogoAtual());
+
+// Static (métodos estáticos)
+console.log("Acionando método estático da classe player", Player.queHorasSao());
+
+// public: atributo acessível de forma geral (dentro e fora da classe)
+// private: atributo é acessível apenas dentro da classe de onde foi criado
+// protected: acessível apenas dentro da classe (e subclasses) onde o atributo foi criado.
+class Game {
+  protected name;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  dizerNome() {
+    return this.name;
+  }
+}
+
+const ghost = new Game("Ghost of Tsushima");
+
+console.log(ghost.dizerNome());
+
+// class GameWithDescription extends Game {
+//   private description;
+
+//   constructor(name: string, description: string) {
+//     super(name);
+
+//     this.description = description;
+//   }
+
+//   dizerNomeComDescricao() {
+//     return `O nome com descrição é ${this.name}`;
+//   }
 // }
 
-// class Player extends User {
-//     constructor(name, age, game) {
-//         super(name, age);
+// Interfaces
 
-//         this.game = game;
+interface IGameWithDescription {
+//   nome: string;
+  descricao: string;
+  dizerNomeComDescricao(): string;
+}
+class GameWithDescription extends Game implements IGameWithDescription {
+  public description;
 
-//     }
-// }
-// const jogador = new Player('Matheus', 20, 'Guardians of the Galaxy');
-// console.log(jogador)
+  constructor(name: string, description: string) {
+    super(name);
+
+    this.description = description;
+  }
+
+  dizerNomeComDescricao() {
+    return `O nome com descrição é ${this.name}`;
+  }
+}
